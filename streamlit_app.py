0088de1736360f518082a97674b4fe714c221e75
suresha-snowflake <104938284+suresha-snowflake@users.noreply.github.com>
2022-05-05 01:33:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text ('Omega 3 and Blueberry Oatmeal')
+streamlit.text('Hard-Boiled Free-Range Egg')
