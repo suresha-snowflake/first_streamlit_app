2bb5883c8c539350015129666d8d380990ed29db
suresha-snowflake <104938284+suresha-snowflake@users.noreply.github.com>
2022-05-05 01:34:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text ('Omega 3 and Blueberry Oatmeal')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
