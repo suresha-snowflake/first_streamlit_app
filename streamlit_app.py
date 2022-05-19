bed549eebe2fa38ff72ff7498d7e9f2f15c88316
suresha-snowflake <104938284+suresha-snowflake@users.noreply.github.com>
2022-05-19 23:37:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ fruit_choice-streamlit.text_input('What fruit would you like information about?'
 streamlit.write('The user entered', fruit_choice)
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-import requests
-fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
+#import requests
+#fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
 #streamlit.text(fruityvice_response.json())
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#streamlit.dataframe(fruityvice_normalized)
