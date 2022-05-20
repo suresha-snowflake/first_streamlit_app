0c3c52ec5c08561604fd776df02eb960937f3743
suresha-snowflake <104938284+suresha-snowflake@users.noreply.github.com>
2022-05-21 01:52:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,4 +30,4 @@ import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized) 
